a3c89c59acd2df8ccfbc002c858ea17e0be92441
Brian Fielder <bfielder@mcneilco.com>
2015-06-11 10:41:18 -0700

    Switched Api Analysis Group PUT routes to use underlying services, not just merge(), and made minor bug fixes to those services.

--- src/main/java/com/labsynch/labseer/domain/AnalysisGroup.java
+++ src/main/java/com/labsynch/labseer/domain/AnalysisGroup.java
@@ -113,14 +113,14 @@ public class AnalysisGroup extends AbstractThing {
 
     public static com.labsynch.labseer.domain.AnalysisGroup update(com.labsynch.labseer.domain.AnalysisGroup analysisGroup) {
         AnalysisGroup updatedAnalysisGroup = AnalysisGroup.findAnalysisGroup(analysisGroup.getId());
-        updatedAnalysisGroup.setRecordedBy(analysisGroup.getRecordedBy());
-        updatedAnalysisGroup.setRecordedDate(analysisGroup.getRecordedDate());
-        updatedAnalysisGroup.setLsTransaction(analysisGroup.getLsTransaction());
-        updatedAnalysisGroup.setModifiedBy(analysisGroup.getModifiedBy());
+        if(analysisGroup.getRecordedBy() != null) updatedAnalysisGroup.setRecordedBy(analysisGroup.getRecordedBy());
+        if(analysisGroup.getRecordedDate() != null) updatedAnalysisGroup.setRecordedDate(analysisGroup.getRecordedDate());
+        if(analysisGroup.getLsTransaction() != null) updatedAnalysisGroup.setLsTransaction(analysisGroup.getLsTransaction());
+        if(analysisGroup.getModifiedBy() != null) updatedAnalysisGroup.setModifiedBy(analysisGroup.getModifiedBy());
         updatedAnalysisGroup.setModifiedDate(new Date());
-        updatedAnalysisGroup.setCodeName(analysisGroup.getCodeName());
-        updatedAnalysisGroup.setLsKind(analysisGroup.getLsKind());
-        updatedAnalysisGroup.setLsType(analysisGroup.getLsType());
+        if(analysisGroup.getRecordedBy() != null) updatedAnalysisGroup.setCodeName(analysisGroup.getRecordedBy());
+        if(analysisGroup.getLsKind() != null) updatedAnalysisGroup.setLsKind(analysisGroup.getLsKind());
+        if(analysisGroup.getLsType() != null) updatedAnalysisGroup.setLsType(analysisGroup.getLsType());
         for (Experiment experiment : analysisGroup.getExperiments()) {
             updatedAnalysisGroup.getExperiments().add(experiment);
         }
