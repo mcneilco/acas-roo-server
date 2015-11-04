266a3aefa4d61a744c0d1c4ce827604e9c1dcb04
Brian Fielder <bfielder@mcneilco.com>
2015-11-04 14:28:54 -0800

    Removed all remaining references to Thing in queries for AnalysisGroupValue data for Data Viewer.

--- src/main/java/com/labsynch/labseer/domain/AnalysisGroupValue.java
+++ src/main/java/com/labsynch/labseer/domain/AnalysisGroupValue.java
@@ -264,12 +264,13 @@ public class AnalysisGroupValue extends AbstractValue {
 	public static TypedQuery<com.labsynch.labseer.dto.AnalysisGroupValueDTO> findAnalysisGroupValueDTO(Set<java.lang.String> batchCodeList) {
 		String sqlQuery = "select new com.labsynch.labseer.dto.AnalysisGroupValueDTO(agv.id, expt.id as experimentId, expt.codeName, el.labelText as prefName, " 
 				+ "agv.lsType as lsType, agv.lsKind as lsKind, " + "agv.stringValue as stringValue, agv.numericValue as numericValue, " 
-				+ "agv2.codeValue AS testedLot, tl.labelText as geneId  " 
-				+ " ) FROM AnalysisGroup ag, LsThing thing " 
+				+ "agv2.codeValue AS testedLot, agv2.codeValue as geneId  " 
+				+ " ) FROM AnalysisGroup ag"
+//				+ ", LsThing thing " 
 				+ "JOIN ag.lsStates ags with ags.lsType = 'data' and ags.ignored = false " 
 				+ "JOIN ags.lsValues agv with agv.lsKind != 'tested concentration' AND agv.lsKind != 'batch code' AND agv.lsKind != 'time' and agv.ignored = false " 
 				+ "JOIN ags.lsValues agv2 with agv2.lsKind = 'batch code' and agv2.ignored = false " 
-				+ "JOIN thing.lsLabels tl with tl.ignored = false and tl.lsType = 'name' and tl.ignored = false and tl.preferred = true " 
+//				+ "JOIN thing.lsLabels tl with tl.ignored = false and tl.lsType = 'name' and tl.ignored = false and tl.preferred = true " 
 				+ "JOIN ag.experiments expt with expt.ignored = false " 
 				+ "JOIN expt.lsLabels el with el.lsType = 'name' and el.lsKind = 'experiment name' and el.preferred = true and el.ignored = false " 
 				+ "WHERE ag.ignored = false AND thing.codeName = agv2.codeValue AND agv2.codeValue IN (:batchCodeList) ";
@@ -288,13 +289,13 @@ public class AnalysisGroupValue extends AbstractValue {
 			Set<String> batchCodeList, boolean publicData) {
 		String sqlQuery = "select new com.labsynch.labseer.dto.AnalysisGroupValueDTO(agv.id, expt.id as experimentId, expt.codeName, el.labelText as prefName, " 
 				+ "agv.lsType as lsType, agv.lsKind as lsKind, " + "agv.stringValue as stringValue, agv.numericValue as numericValue, " 
-				+ "agv2.codeValue AS testedLot, tl.labelText as geneId  " 
+				+ "agv2.codeValue AS testedLot, agv2.codeValue as geneId  " 
 				+ " ) FROM AnalysisGroup ag " 
 				+ "JOIN ag.lsStates ags with ags.lsType = 'data' and ags.ignored = false " 
 				+ "JOIN ags.lsValues agv with agv.lsKind != 'tested concentration' AND agv.lsKind != 'batch code' AND agv.lsKind != 'time' and agv.ignored = false " 
 				+ "JOIN ags.lsValues agv2 with agv2.lsKind = 'batch code' and agv2.ignored = false " 
-				+ "LEFT OUTER JOIN LsThing thing with thing.codeName = agv2.codeValue "
-				+ "LEFT OUTER JOIN thing.lsLabels tl with tl.ignored = false and tl.lsType = 'name' and tl.ignored = false and tl.preferred = true  " 
+//				+ "LEFT OUTER JOIN LsThing thing with thing.codeName = agv2.codeValue "
+//				+ "LEFT OUTER JOIN thing.lsLabels tl with tl.ignored = false and tl.lsType = 'name' and tl.ignored = false and tl.preferred = true  " 
 				+ "JOIN ag.experiments expt with expt.ignored = false " 
 				+ "JOIN expt.lsLabels el with el.lsType = 'name' and el.lsKind = 'experiment name' and el.preferred = true and el.ignored = false " 
 				+ "WHERE ag.ignored = false AND agv2.codeValue IN (:batchCodeList) AND agv.publicData = :publicData  ";
@@ -497,11 +498,12 @@ public class AnalysisGroupValue extends AbstractValue {
 	public static TypedQuery<com.labsynch.labseer.dto.AnalysisGroupValueDTO> findAnalysisGroupValueDTOByExperiments(Set<java.lang.String> experimentCodeList, boolean publicData) {
 		String sqlQuery = "select new com.labsynch.labseer.dto.AnalysisGroupValueDTO(agv.id, expt.id as experimentId, expt.codeName, el.labelText as prefName, " 
 	+ "agv.lsType as lsType, agv.lsKind as lsKind, " + "agv.stringValue as stringValue, agv.numericValue as numericValue, agv.codeValue as codeValue, agv.fileValue as fileValue, " 
-				+ "agv2.codeValue AS testedLot, tl.labelText as geneId " + " ) FROM AnalysisGroup ag, LsThing thing " 
+				+ "agv2.codeValue AS testedLot, agv2.codeValue as geneId " + " ) FROM AnalysisGroup ag"
+//						+ ", LsThing thing " 
 	+ "JOIN ag.lsStates ags with ags.lsType = 'data' and ags.ignored = false " 
 				+ "JOIN ags.lsValues agv with agv.lsKind != 'tested concentration' AND agv.lsKind != 'batch code' AND agv.lsKind != 'time' and agv.ignored = false " 
 	+ "JOIN ags.lsValues agv2 with agv2.lsKind = 'batch code' and agv2.ignored = false " 
-				+ "JOIN thing.lsLabels tl with tl.ignored = false and tl.lsType = 'name' and tl.lsKind = 'Entrez Gene ID' and tl.ignored = false and tl.preferred = true " 
+//				+ "JOIN thing.lsLabels tl with tl.ignored = false and tl.lsType = 'name' and tl.lsKind = 'Entrez Gene ID' and tl.ignored = false and tl.preferred = true " 
 	+ "JOIN ag.experiments expt with expt.ignored = false " + "JOIN expt.lsLabels el with el.lsType = 'name' and el.lsKind = 'experiment name' and el.preferred = true and el.ignored = false " 
 				+ "WHERE ag.ignored = false " + "AND thing.codeName = agv2.codeValue AND agv.publicData = :publicData " + "AND expt.codeName IN  (:experimentCodeList) ";
 		logger.debug("query sql: " + sqlQuery);
