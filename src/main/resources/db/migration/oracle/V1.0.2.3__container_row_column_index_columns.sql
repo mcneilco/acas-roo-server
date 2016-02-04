c2123f0c9aaa2006e2b83121ceb40ed1ff4512e2
Brian Fielder <bfielder@mcneilco.com>
2016-02-04 12:36:12 -0800

    Changed column type to specify NUMBER(10,0) to match Roo-autogenerated datatype for java.Integer

--- src/main/resources/db/migration/oracle/V1.0.2.3__container_row_column_index_columns.sql
+++ src/main/resources/db/migration/oracle/V1.0.2.3__container_row_column_index_columns.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "ACAS"."CONTAINER" ADD (column_index INTEGER,
-							row_index INTEGER);
+ALTER TABLE "ACAS"."CONTAINER" ADD (column_index NUMBER(10,0),
+							row_index NUMBER(10,0));
