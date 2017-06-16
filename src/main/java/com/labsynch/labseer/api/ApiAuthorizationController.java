d3b7df0046e4deadfd4524ec09ddf4ebf66324dc
goshiro2013 <guy@mcneilco.com>
2017-06-16 10:53:37 -0700

    remove extraneous security messageDigest imports

--- src/main/java/com/labsynch/labseer/api/ApiAuthorizationController.java
+++ src/main/java/com/labsynch/labseer/api/ApiAuthorizationController.java
@@ -1,9 +1,6 @@
 package com.labsynch.labseer.api;
 
 import java.util.Collection;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,26 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.encoding.MessageDigestPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.labsynch.labseer.domain.Author;
-import com.labsynch.labseer.domain.AuthorRole;
-import com.labsynch.labseer.domain.LsRole;
 import com.labsynch.labseer.domain.LsThing;
 import com.labsynch.labseer.dto.AuthGroupsAndProjectsDTO;
-import com.labsynch.labseer.dto.AuthorNameDTO;
 import com.labsynch.labseer.dto.CodeTableDTO;
 import com.labsynch.labseer.service.AuthorService;
-import com.labsynch.labseer.utils.PropertiesFileService;
-import com.labsynch.labseer.utils.PropertiesUtilService;
-import com.labsynch.labseer.web.AuthorController;
 
 @Controller
 @RequestMapping("api/v1/authorization")
