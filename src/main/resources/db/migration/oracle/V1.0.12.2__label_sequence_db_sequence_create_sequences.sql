45828258506d66259ef13e213730bda5adad191d
Brian Frost <bfrost@mcneilco.com>
2017-11-27 11:39:53 -0500

    For flyway label sequence migration 1.0.12.2: Fixed loop structure and changed db_sequence autogeneration to include the label_sequence.id to avoid duplicates on Oracle that were causing the migration to fail. For migration 1.0.13.1: Fixed syntax on Oracle conditional unique indexes for author email and username.

--- src/main/resources/db/migration/oracle/V1.0.12.2__label_sequence_db_sequence_create_sequences.sql
+++ src/main/resources/db/migration/oracle/V1.0.12.2__label_sequence_db_sequence_create_sequences.sql
@@ -8,19 +8,21 @@ BEGIN
 	  labelseq labelseqtype;
 	BEGIN
 	  for labelseq in
-			(SELECT id as label_sequence_id, SUBSTR(COALESCE(db_sequence, regexp_replace(('labelseq_' || label_prefix || '_' || label_type_and_kind || '_' || thing_type_and_kind ), '[^a-zA-Z_]+', '_')), 0, 29) as db_sequence_name, GREATEST(starting_number, 1) as starting_num FROM label_sequence)
+			(SELECT id as label_sequence_id, SUBSTR(COALESCE(db_sequence, regexp_replace(( 'labelseq_' || TO_CHAR(id) || '_' || label_prefix || '_' || label_type_and_kind || '_' || thing_type_and_kind ), '[^0-9a-zA-Z_]+', '_')), 0, 29) as db_sequence_name, GREATEST(starting_number, 1) as starting_num FROM label_sequence)
 		loop
+		BEGIN
         	dbms_output.Put_line('Creating new sequence: ' || labelseq.db_sequence_name || ' starting with '||labelseq.starting_num);
 			EXECUTE IMMEDIATE 'CREATE SEQUENCE '||labelseq.db_sequence_name||' START WITH '|| labelseq.starting_num;
 		  	EXECUTE IMMEDIATE 'UPDATE label_sequence SET db_sequence = :dbseqname WHERE id = :labelseqid' USING labelseq.db_sequence_name, labelseq.label_sequence_id;
-		END loop;
-	EXCEPTION
-    WHEN OTHERS THEN
-      IF SQLCODE = -955 THEN
-        NULL;
-      ELSE
-        RAISE;
-      END IF;
+		EXCEPTION
+   			 WHEN OTHERS THEN
+      		 IF SQLCODE = -955 THEN
+      		  CONTINUE;
+     		ELSE
+       			 CONTINUE;
+     		 END IF;
+     	END;
+     	END loop;
 	END;
 
 	BEGIN
