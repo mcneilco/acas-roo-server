45a7a8d7fb1e4cba0f357758dd219a7da0f4c793
Brian Bolt <brian.bolt@schrodinger.com>
2022-04-05 15:33:22 -0700

    Add firewall rule to allow doubleslash

---
+++ src/main/java/com/labsynch/labseer/utils/ACASHttpFirewall.java
@@ -0,0 +1,11 @@
+package com.labsynch.labseer.utils;
+
+
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+
+public class ACASHttpFirewall extends DefaultHttpFirewall {
+    ACASHttpFirewall() {
+        this.setAllowUrlEncodedSlash(true);
+    }
+    
+}
