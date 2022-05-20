d8f0d0341c94bd3a5a717fd1675151acc9db20b0
Brian Frost <brian.frost@schrodinger.com>
2022-05-20 14:50:15 -0700

    add validateBeforeSave, DRY up some repetition

--- src/main/java/com/labsynch/labseer/api/ApiPhysicalStateController.java
+++ src/main/java/com/labsynch/labseer/api/ApiPhysicalStateController.java
@@ -1,8 +1,15 @@
 package com.labsynch.labseer.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 import com.labsynch.labseer.domain.PhysicalState;
+import com.labsynch.labseer.service.ErrorMessage;
 import com.labsynch.labseer.utils.PropertiesUtilService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -20,13 +27,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ApiPhysicalStateController {
 
+    Logger logger = LoggerFactory.getLogger(ApiPhysicalStateController.class);
+
     @Autowired
     private PropertiesUtilService propertiesUtilService;
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
-    @ResponseBody
-    public ResponseEntity<String> showJson(@PathVariable("id") Long id) {
-        PhysicalState physicalstate = PhysicalState.findPhysicalState(id);
+    private static HttpHeaders getTextHeaders() {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/text; charset=utf-8");
         headers.add("Access-Control-Allow-Origin", "*");
@@ -34,6 +40,25 @@ public class ApiPhysicalStateController {
         headers.add("Cache-Control", "no-store, no-cache, must-revalidate"); // HTTP 1.1
         headers.add("Pragma", "no-cache"); // HTTP 1.0
         headers.setExpires(0); // Expire the cache
+        return headers;
+    }
+
+    private static HttpHeaders getJsonHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/json; charset=utf-8");
+		headers.add("Access-Control-Allow-Headers", "Content-Type");
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Cache-Control", "no-store, no-cache, must-revalidate"); // HTTP 1.1
+		headers.add("Pragma", "no-cache"); // HTTP 1.0
+		headers.setExpires(0); // Expire the cache
+        return headers;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+    @ResponseBody
+    public ResponseEntity<String> showJson(@PathVariable("id") Long id) {
+        PhysicalState physicalstate = PhysicalState.findPhysicalState(id);
+        HttpHeaders headers = getTextHeaders();
         if (physicalstate == null) {
             return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
         }
@@ -43,13 +68,7 @@ public class ApiPhysicalStateController {
     @RequestMapping(headers = "Accept=application/json")
     @ResponseBody
     public ResponseEntity<String> listJson() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/text; charset=utf-8");
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Headers", "Content-Type");
-        headers.add("Cache-Control", "no-store, no-cache, must-revalidate"); // HTTP 1.1
-        headers.add("Pragma", "no-cache"); // HTTP 1.0
-        headers.setExpires(0); // Expire the cache
+        HttpHeaders headers = getTextHeaders();
 
         if (propertiesUtilService.getOrderSelectLists()) {
             return new ResponseEntity<String>(
@@ -65,10 +84,6 @@ public class ApiPhysicalStateController {
     public ResponseEntity<String> createFromJson(@RequestBody String json) {
         PhysicalState ps = PhysicalState.fromJsonToPhysicalState(json);
         ps.persist();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/text");
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Headers", "Content-Type");
         return showJson(ps.getId());
     }
 
@@ -77,19 +92,13 @@ public class ApiPhysicalStateController {
         for (PhysicalState physicalState : PhysicalState.fromJsonArrayToPhysicalStates(json)) {
             physicalState.persist();
         }
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/text");
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Headers", "Content-Type");
+        HttpHeaders headers = getTextHeaders();
         return new ResponseEntity<String>(headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.PUT, headers = "Accept=application/json")
     public ResponseEntity<String> updateFromJson(@RequestBody String json) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/text");
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Headers", "Content-Type");
+        HttpHeaders headers = getTextHeaders();
         if (PhysicalState.fromJsonToPhysicalState(json).merge() == null) {
             return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
         }
@@ -98,10 +107,7 @@ public class ApiPhysicalStateController {
 
     @RequestMapping(value = "/jsonArray", method = RequestMethod.PUT, headers = "Accept=application/json")
     public ResponseEntity<String> updateFromJsonArray(@RequestBody String json) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/text");
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Headers", "Content-Type");
+        HttpHeaders headers = getTextHeaders();
         for (PhysicalState physicalState : PhysicalState.fromJsonArrayToPhysicalStates(json)) {
             if (physicalState.merge() == null) {
                 return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
@@ -113,10 +119,7 @@ public class ApiPhysicalStateController {
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
     public ResponseEntity<String> deleteFromJson(@PathVariable("id") Long id) {
         PhysicalState physicalstate = PhysicalState.findPhysicalState(id);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/text");
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Headers", "Content-Type");
+        HttpHeaders headers = getTextHeaders();
         if (physicalstate == null) {
             return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
         }
@@ -126,14 +129,50 @@ public class ApiPhysicalStateController {
 
     @RequestMapping(method = RequestMethod.OPTIONS)
     public ResponseEntity<String> getPhysicalStateOptions() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/text");
-        headers.add("Access-Control-Allow-Headers", "Content-Type");
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Cache-Control", "no-store, no-cache, must-revalidate"); // HTTP 1.1
-        headers.add("Pragma", "no-cache"); // HTTP 1.0
-        headers.setExpires(0); // Expire the cache
+        HttpHeaders headers = getTextHeaders();
         return new ResponseEntity<String>(headers, HttpStatus.OK);
     }
+
+    // validate physicalState before saving
+	// is physicalState code still unique?
+	// is physicalState name unique? (optional)
+	@RequestMapping(value = "/validateBeforeSave", method = RequestMethod.POST, headers = "Accept=application/json")
+	@ResponseBody
+	public ResponseEntity<String> validateNewPhysicalState(@RequestBody String json) {
+		logger.info("validateNewPhysicalState -- incoming json: " + json);
+		PhysicalState queryPhysicalState = PhysicalState.fromJsonToPhysicalState(json);
+		if (queryPhysicalState.getCode() == null || queryPhysicalState.getCode().equalsIgnoreCase("")) {
+			logger.info("creating the missing code name");
+			queryPhysicalState.setCode(queryPhysicalState.getName().toLowerCase());
+		}
+		logger.info("validateNewPhysicalState -- query physicalState: " + queryPhysicalState.toJson());
+
+		ArrayList<ErrorMessage> errors = new ArrayList<ErrorMessage>();
+		// boolean errorsFound = false;
+
+		HttpHeaders headers = getJsonHeaders(); // Expire the cache
+
+		Long physicalStateByCodeCount = 0L;
+		List<PhysicalState> queryPhysicalStates = PhysicalState.findPhysicalStatesByCodeEquals(queryPhysicalState.getCode()).getResultList();
+		for (PhysicalState physicalState : queryPhysicalStates) {
+			if (queryPhysicalState.getId() == null || physicalState.getId().longValue() != queryPhysicalState.getId().longValue()) {
+				++physicalStateByCodeCount;
+				logger.debug("current physicalState: " + physicalState.toJson());
+				logger.debug("physicalState id: " + physicalState.getId());
+				logger.debug("query physicalState ID: " + queryPhysicalState.getId());
+				logger.debug("incrementing the physicalState count " + physicalStateByCodeCount);
+			}
+		}
+
+		if (physicalStateByCodeCount > 0) {
+			ErrorMessage error = new ErrorMessage();
+			error.setLevel("ERROR");
+			error.setMessage("Found another physicalState with the same code name");
+			errors.add(error);
+			return new ResponseEntity<String>(ErrorMessage.toJsonArray(errors), headers, HttpStatus.CONFLICT);
+		} else {
+			return new ResponseEntity<String>(queryPhysicalState.toJson(), headers, HttpStatus.OK);
+		}
+	}
 
 }
