12a85ab76961956544b2e867f078ace0cbb1aaa4
Brian Bolt <brian.bolt@schrodinger.com>
2022-10-27 20:58:06 +0000

    Truncate settings as part of flyway

--- src/main/resources/db/migration/postgres/V2.4.0.5__standardization_settings_reasons.sql
+++ src/main/resources/db/migration/postgres/V2.4.0.5__standardization_settings_reasons.sql
@@ -3,3 +3,4 @@ ALTER TABLE standardization_settings ADD COLUMN IF NOT EXISTS valid boolean;
 ALTER TABLE standardization_settings ADD COLUMN IF NOT EXISTS invalid_reasons text;
 ALTER TABLE standardization_settings ADD COLUMN IF NOT EXISTS needs_standardization_reasons text;
 ALTER TABLE standardization_settings ADD COLUMN IF NOT EXISTS suggested_configuration_changes text;
+TRUNCATE standardization_settings;
