45fb17b4a167d975aaa8b9dbe00fd5b630cd525d
Philip Cheung <philip@refactorbio.com>
2022-11-03 13:21:58 -0700

    Made it a pure copy of Brian's migration code

--- src/main/java/com/labsynch/labseer/db/migration/postgres/jchem/V2_4_0_6__Upgrade_Jchem_Tables.java
+++ src/main/java/com/labsynch/labseer/db/migration/postgres/jchem/V2_4_0_6__Upgrade_Jchem_Tables.java
@@ -1,24 +1,22 @@
 package com.labsynch.labseer.db.migration.postgres.jchem;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.sql.SQLException;
 
-import chemaxon.util.ConnectionHandler;
-import chemaxon.jchem.db.Updater;
 import chemaxon.jchem.db.UpdateHandlerException;
+import chemaxon.jchem.db.Updater;
+import chemaxon.util.ConnectionHandler;
 
 public class V2_4_0_6__Upgrade_Jchem_Tables implements JdbcMigration {
 
 	Logger logger = LoggerFactory.getLogger(V2_4_0_6__Upgrade_Jchem_Tables.class);
 
-	// Updating jchem version to 22.12.0
-
 	public void migrate(Connection conn) throws Exception {
-		logger.info("ATTEMPTING TO UPGRADE JCHEM TABLES for jchem version to 22.12.0");
+		logger.info("ATTEMPTING TO UPGRADE JCHEM TABLES");
 		conn.setAutoCommit(true);
 		logger.info("connection autocommit mode: " + conn.getAutoCommit());
 		logger.info("getTransactionIsolation  " + conn.getTransactionIsolation());
