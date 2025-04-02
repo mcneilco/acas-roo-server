33462eb9e36b5bf0d60cd4bf469ce61d873539d9
Brian Bolt <brian.bolt@schrodinger.com>
2025-04-02 10:16:10 -0400

    parent id unique index

--- src/main/resources/db/migration/postgres/V2.4.2.8__standardization_dry_run_indexes.sql
+++ src/main/resources/db/migration/postgres/V2.4.2.8__standardization_dry_run_indexes.sql
@@ -5,3 +5,6 @@ CREATE INDEX IF NOT EXISTS stndzn_dry_run_registration_status_idx on standardiza
 CREATE INDEX IF NOT EXISTS stndzn_dry_run_existing_duplicate_count_idx on standardization_dry_run_compound(existing_duplicate_count);
 CREATE INDEX IF NOT EXISTS stndzn_dry_run_new_duplicate_count_idx on standardization_dry_run_compound(new_duplicate_count);
 CREATE INDEX IF NOT EXISTS stndzn_dry_run_changed_structure_idx on standardization_dry_run_compound(changed_structure);
+
+-- Create a unique constraint on parent_id
+CREATE UNIQUE INDEX IF NOT EXISTS stndzn_dry_run_parent_id_unique_idx on standardization_dry_run_compound(parent_id) where parent_id is not null;
